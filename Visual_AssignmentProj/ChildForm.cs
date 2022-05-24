bd06cc68e395754e2a0775687dbbeb0d1695ed79
AlandIbrahim <92864566+AlandIbrahim@users.noreply.github.com>
2022-05-24 20:20:01 +0300

    Add project files.

---
+++ Visual_AssignmentProj/ChildForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_AssignmentProj
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+
+
+            //extra stuff (for fun):
+            unchecked
+            {
+                //generate "random" object
+                Random r= new Random();
+                //assign an integer a random value from its lowest value to its highest.
+                int val=r.Next(int.MinValue, int.MaxValue);
+                //use "|=" aka "OR" to add full opacity to the color (because backColor doesn't support transparency)
+                val |= (int)0xff000000;
+                //use our processed value to create a color from Argb color system and assign it to the BackColor.
+            BackColor = Color.FromArgb(val);
+            }
+            //end of the extra stuff.
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Visual_AssignmentProj/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_AssignmentProj
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm CF = new ChildForm
+            {
+                MdiParent = this
+            };
+            CF.Show();
+        }
+
+        private void newFreeWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm CF = new ChildForm();
+            CF.ShowDialog();
+        }
+
+        private void closeAllMDIChildrenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (var item in MdiChildren)
+            {
+                item.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void verticallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void horizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+    }
+}
